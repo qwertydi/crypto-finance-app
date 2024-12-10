408244e51cb1e94ae8ae35cb80aa5d7dbac7ed23
Diogo Correia <diogocorreia72@hotmail.com>
2024-12-10 22:14:19 +0000

    Setup multi module repository

---
+++ crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/CryptoFinanceServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dmsc.cryptofinanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoFinanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoFinanceServiceApplication.class, args);
+    }
+}
---
+++ crypto-finance-service/src/test/java/com/dmsc/cryptofinanceservice/CryptoFinanceServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.dmsc.cryptofinanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoFinanceServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
