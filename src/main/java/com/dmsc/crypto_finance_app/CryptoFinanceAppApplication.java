c3344ffabc29eb615ee83ae5a26712030bddac72
Diogo Correia <diogocorreia1996@gmail.com>
2024-12-10 21:56:27 +0000

    Project Setup

---
+++ src/main/java/com/dmsc/crypto_finance_app/CryptoFinanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.dmsc.crypto_finance_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoFinanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoFinanceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmsc/crypto_finance_app/CryptoFinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmsc.crypto_finance_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoFinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
