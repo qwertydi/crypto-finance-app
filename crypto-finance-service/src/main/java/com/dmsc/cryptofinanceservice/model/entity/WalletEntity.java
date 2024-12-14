83a442f1b20ed9e7bf642aca458e773487f51589
Diogo Correia <diogocorreia72@hotmail.com>
2024-12-14 17:50:51 +0000

    Store wallet asset
    - Create entity, repository and update services to pass the assets to be associated to the wallet

--- crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/model/entity/WalletEntity.java
+++ crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/model/entity/WalletEntity.java
@@ -6,11 +6,14 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -25,4 +28,7 @@ public class WalletEntity {
 
     @OneToOne(mappedBy = "wallet", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private WalletJobEntity walletJob;
+
+    @OneToMany(mappedBy = "wallet", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WalletAssetEntity> coins = new ArrayList<>();
 }
--- crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/service/WalletService.java
+++ crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/service/WalletService.java
@@ -2,14 +2,19 @@ package com.dmsc.cryptofinanceservice.service;
 
 import com.dmsc.cryptofinanceservice.exception.WalletNotFoundException;
 import com.dmsc.cryptofinanceservice.model.dto.WalletDto;
+import com.dmsc.cryptofinanceservice.model.entity.WalletAssetEntity;
 import com.dmsc.cryptofinanceservice.model.entity.WalletEntity;
 import com.dmsc.cryptofinanceservice.model.entity.WalletJobEntity;
+import com.dmsc.cryptofinanceservice.model.rest.CreateWalletRequest;
+import com.dmsc.cryptofinanceservice.repository.WalletAssetRepository;
 import com.dmsc.cryptofinanceservice.repository.WalletJobRepository;
 import com.dmsc.cryptofinanceservice.repository.WalletRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,20 +22,37 @@ public class WalletService {
 
     private final WalletRepository walletRepository;
     private final WalletJobRepository walletJobRepository;
+    private final WalletAssetRepository walletAssetRepository;
     private final ModelMapper modelMapper;
 
-    public WalletService(WalletRepository walletRepository, WalletJobRepository walletJobRepository) {
+    public WalletService(WalletRepository walletRepository,
+                         WalletJobRepository walletJobRepository,
+                         WalletAssetRepository walletAssetRepository) {
         this.walletRepository = walletRepository;
         this.walletJobRepository = walletJobRepository;
+        this.walletAssetRepository = walletAssetRepository;
         this.modelMapper = new ModelMapper();
     }
 
-    public WalletDto createWallet(Duration frequency) {
+    public WalletDto createWallet(Duration frequency, CreateWalletRequest request) {
         WalletEntity wallet = walletRepository.save(new WalletEntity());
         WalletJobEntity walletJobEntity = new WalletJobEntity();
         walletJobEntity.setWallet(wallet);
         walletJobEntity.setFrequency(frequency);
         walletJobRepository.save(walletJobEntity);
+
+        List<WalletAssetEntity> assetList = new ArrayList<>();
+        // external id and name missing, update on API call (?)
+        request.getWallet()
+            .forEach(asset -> {
+                WalletAssetEntity assetEntity = new WalletAssetEntity();
+                assetEntity.setSymbol(asset.getSymbol());
+                assetEntity.setPrice(asset.getPrice());
+                assetEntity.setQuantity(asset.getQuantity());
+                assetEntity.setWallet(wallet);
+                assetList.add(assetEntity);
+            });
+        walletAssetRepository.saveAll(assetList);
         return modelMapper.map(wallet, WalletDto.class);
     }
 
