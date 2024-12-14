34746e14a0a0248a9d9b7f408b1cec1294e9238b
Diogo Correia <diogocorreia72@hotmail.com>
2024-12-14 17:21:26 +0000

    Wallet entities and repository

---
+++ crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/repository/WalletRepository.java
@@ -0,0 +1,11 @@
+package com.dmsc.cryptofinanceservice.repository;
+
+import com.dmsc.cryptofinanceservice.model.entity.WalletEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface WalletRepository extends JpaRepository<WalletEntity, UUID> {
+}
