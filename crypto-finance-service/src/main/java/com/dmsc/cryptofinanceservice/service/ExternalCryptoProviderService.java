c77016e7d4bb768d8576d569f7210dedd09b333a
Diogo Correia <diogocorreia72@hotmail.com>
2024-12-14 19:12:31 +0000

    Allow to fetch Crypto price by asset id and symbol

--- crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/service/ExternalCryptoProviderService.java
+++ crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/service/ExternalCryptoProviderService.java
@@ -2,14 +2,17 @@ package com.dmsc.cryptofinanceservice.service;
 
 import com.dmsc.coincapjavasdk.WebClientAssetsRestSdk;
 import com.dmsc.coincapjavasdk.model.request.CryptoDataRequest;
+import com.dmsc.coincapjavasdk.model.response.DataDetails;
 import com.dmsc.cryptofinanceservice.model.dto.CryptoItemDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Slf4j
 @Service
@@ -23,12 +26,24 @@ public class ExternalCryptoProviderService implements CryptoProvider {
 
     @Override
     public Mono<List<CryptoItemDto>> getAssetsBySymbols(List<String> listSymbols) {
+        return getAssets(listSymbols, null);
+    }
+
+    private Mono<List<CryptoItemDto>> getAssets(List<String> listSymbols, List<String> listIds) {
         CryptoDataRequest request = new CryptoDataRequest();
-        request.setSearch(listSymbols);
+        if (!CollectionUtils.isEmpty(listSymbols)) {
+            request.setSearch(listSymbols);
+        }
+        if (!CollectionUtils.isEmpty(listIds)) {
+            request.setIds(listIds);
+        }
+
+        Predicate<DataDetails> dataDetailsPredicate = !CollectionUtils.isEmpty(listSymbols) ?
+            item -> listSymbols.contains(item.getSymbol()) : item -> true;
 
         return assetsReactiveSdk.getAssetsAsync(request)
             .map(response -> response.getData().stream()
-                .filter(item -> listSymbols.contains(item.getSymbol()))
+                .filter(dataDetailsPredicate)
                 .map(item -> CryptoItemDto.builder()
                     .id(item.getId())
                     .name(item.getName())
@@ -37,5 +52,10 @@ public class ExternalCryptoProviderService implements CryptoProvider {
                     .timestamp(Instant.ofEpochMilli(response.getTimestamp()))
                     .build())
                 .toList());
+    }
+
+    @Override
+    public Mono<List<CryptoItemDto>> getAssetsById(List<String> id) {
+        return getAssets(null, id);
     }
 }
