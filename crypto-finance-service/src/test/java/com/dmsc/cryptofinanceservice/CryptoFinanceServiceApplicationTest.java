c0be5866bcd3ad4e440ee836897f270ffc982662
Diogo Correia <diogocorreia72@hotmail.com>
2024-12-14 17:37:10 +0000

    Skip @SpringBootTest

--- crypto-finance-service/src/test/java/com/dmsc/cryptofinanceservice/CryptoFinanceServiceApplicationTest.java
+++ crypto-finance-service/src/test/java/com/dmsc/cryptofinanceservice/CryptoFinanceServiceApplicationTest.java
@@ -1,9 +1,8 @@
 package com.dmsc.cryptofinanceservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CryptoFinanceServiceApplicationTest {
 
     @Test
