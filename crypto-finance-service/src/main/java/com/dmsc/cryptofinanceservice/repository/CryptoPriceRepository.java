a4f01fbb5fee65752110ba217b3967d27c8fca38
Diogo Correia <diogocorreia72@hotmail.com>
2024-12-15 11:44:14 +0000

    Update CryptoPriceRepository.java to support existing and latest crypto prices from Database

--- crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/repository/CryptoPriceRepository.java
+++ crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/repository/CryptoPriceRepository.java
@@ -4,6 +4,12 @@ import com.dmsc.cryptofinanceservice.model.entity.CryptoPriceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.Optional;
+
 @Repository
 public interface CryptoPriceRepository extends JpaRepository<CryptoPriceEntity, Long> {
+    Optional<CryptoPriceEntity> findTopByExternalIdOrderByTimeDesc(String externalId);
+
+    Optional<CryptoPriceEntity> findTopByExternalIdAndTimeOrderByTimeDesc(String externalId, Instant time);
 }
