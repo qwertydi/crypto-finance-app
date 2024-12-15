23f58f024374cd0704d4868809d8cc48ce461461
Diogo Correia <diogocorreia72@hotmail.com>
2024-12-15 15:08:07 +0000

    Add cache service to CryptoPriceService
    - Update cache on new entry
    - Update cache on startup

--- crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/repository/CryptoPriceRepository.java
+++ crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/repository/CryptoPriceRepository.java
@@ -1,7 +1,11 @@
 package com.dmsc.cryptofinanceservice.repository;
 
 import com.dmsc.cryptofinanceservice.model.entity.CryptoPriceEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
@@ -11,5 +15,20 @@ import java.util.Optional;
 public interface CryptoPriceRepository extends JpaRepository<CryptoPriceEntity, Long>, CryptoPriceRepositoryCustom {
     Optional<CryptoPriceEntity> findTopByExternalIdOrderByTimeDesc(String externalId);
 
-    Optional<CryptoPriceEntity> findTopByExternalIdAndTimeOrderByTimeDesc(String externalId, Instant time);
+    /**
+     * Find the first price for an externalId at the second mark and not full instant
+     *
+     * @param externalId String
+     * @param time       Instant
+     * @return Optional<CryptoPriceEntity>
+     */
+    @Query("SELECT c FROM crypto_price c " +
+        "WHERE c.externalId = :externalId " +
+        "AND DATE_TRUNC('second', c.time) = DATE_TRUNC('second', CAST(:time AS TIMESTAMP)) " +
+        "ORDER BY c.time DESC")
+    Page<CryptoPriceEntity> findTopByExternalIdAndTimeOrderByTimeDesc(
+        @Param("externalId") String externalId,
+        @Param("time") Instant time,
+        Pageable pageable
+    );
 }
