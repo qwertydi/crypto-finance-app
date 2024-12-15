45a5fd0ecf88cc80a68794db9a6e7a302d8bc616
Diogo Correia <diogocorreia72@hotmail.com>
2024-12-15 16:01:10 +0000

    Support WalletJobRepository to find job by walletId

--- crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/repository/WalletJobRepository.java
+++ crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/repository/WalletJobRepository.java
@@ -4,6 +4,10 @@ import com.dmsc.cryptofinanceservice.model.entity.WalletJobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface WalletJobRepository extends JpaRepository<WalletJobEntity, Long> {
+    Optional<WalletJobEntity> findByWalletId(UUID id);
 }
