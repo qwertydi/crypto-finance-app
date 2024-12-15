b1cc53d7033312cba9d615674b91cce730888de0
Diogo Correia <diogocorreia72@hotmail.com>
2024-12-15 12:04:01 +0000

    Create endpoint to fetch wallet details

--- crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/controller/WalletController.java
+++ crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/controller/WalletController.java
@@ -2,8 +2,11 @@ package com.dmsc.cryptofinanceservice.controller;
 
 import com.dmsc.cryptofinanceservice.model.rest.CreateWalletRequest;
 import com.dmsc.cryptofinanceservice.model.rest.CreateWalletResponse;
+import com.dmsc.cryptofinanceservice.model.rest.WalletResponse;
 import com.dmsc.cryptofinanceservice.service.WalletAggregatorService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(WalletController.BASE_URL)
@@ -31,5 +36,11 @@ public class WalletController {
     @PostMapping
     public ResponseEntity<CreateWalletResponse> walletInfo(@RequestBody String request, @RequestParam(value = "frequency", required = false) Duration frequency) {
         return ResponseEntity.ok(walletAggregatorService.createWallet(request, frequency));
+    }
+
+    @GetMapping
+    public ResponseEntity<WalletResponse> getWalletInfo(@RequestParam(value = "walletId") String walletId,
+                                                        @RequestParam(value = "date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Instant date) {
+        return ResponseEntity.ok(walletAggregatorService.fetchWalletInfo(walletId, Optional.ofNullable(date)));
     }
 }
