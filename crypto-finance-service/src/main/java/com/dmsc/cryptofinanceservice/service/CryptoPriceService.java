419f15de0e76ff6dea15fac76a46bc5a616a0673
Diogo Correia <diogocorreia72@hotmail.com>
2024-12-15 12:01:47 +0000

    Support parallel and concurrent requests per walletId not exceeding max number of threads per wallet

--- crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/service/CryptoPriceService.java
+++ crypto-finance-service/src/main/java/com/dmsc/cryptofinanceservice/service/CryptoPriceService.java
@@ -2,14 +2,26 @@ package com.dmsc.cryptofinanceservice.service;
 
 import com.dmsc.cryptofinanceservice.exception.AssetDataNotFound;
 import com.dmsc.cryptofinanceservice.model.dto.CryptoItemDto;
+import com.dmsc.cryptofinanceservice.model.dto.WalletAssetDto;
 import com.dmsc.cryptofinanceservice.model.entity.CryptoPriceEntity;
+import com.dmsc.cryptofinanceservice.properties.WalletRequestProperties;
 import com.dmsc.cryptofinanceservice.repository.CryptoPriceRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Service
@@ -19,12 +31,21 @@ public class CryptoPriceService {
     private final CryptoPriceRepository cryptoPriceRepository;
     private final CryptoProvider cryptoProvider;
 
+    // configurable value for max number of threads per wallet
+    private final int numberOfThreadsPerWallet;
+
+    // Handle multiple simultaneous access to the hashmap
+    private final Map<UUID, Queue<WalletAssetDto>> walletQueue = new ConcurrentHashMap<>();
+
     public CryptoPriceService(WalletAssetService walletAssetService,
                               CryptoPriceRepository cryptoPriceRepository,
-                              CryptoProvider cryptoProvider) {
+                              CryptoProvider cryptoProvider,
+                              WalletRequestProperties walletRequestProperties) {
         this.walletAssetService = walletAssetService;
         this.cryptoPriceRepository = cryptoPriceRepository;
         this.cryptoProvider = cryptoProvider;
+        this.numberOfThreadsPerWallet = walletRequestProperties.getNumberOfThreads();
+    }
 
     private static CryptoItemDto getCryptoItemDto(CryptoPriceEntity cryptoPriceEntity) {
         return CryptoItemDto.builder()
@@ -77,17 +98,85 @@ public class CryptoPriceService {
         return save;
     }
 
+    /**
+     * Method will use the walletId to fetch the Queue with {@link WalletAssetDto} and process the items in parallel manner
+     *
+     * @param walletId UUID
+     */
     public void fetchWalletPrices(UUID walletId) {
-        walletAssetService.findWalletAssetsByWalletId(walletId)
-            .forEach(asset -> {
-                // improvement: add a cache of symbol to external id
-                if (StringUtils.hasText(asset.getExternalId())) {
-                    updateCryptoPrice(asset.getExternalId());
-                } else {
-                    updateCryptoPriceBySymbol(walletId, asset.getId(), asset.getSymbol());
-                    log.debug("No externalId for {}, walletId: {}", asset.getSymbol(), walletId);
-                }
-            });
+        fetchWalletPricesManuallyTriggered(walletId, walletAssetService.findWalletAssetsByWalletId(walletId));
+    }
+
+    /**
+     * Manually add List of {@link WalletAssetDto} for a specific wallet queue.
+     * The queue for the wallet will then be processed in batch.
+     *
+     * @param walletId         WalletId
+     * @param listWalletAssets List of {@link WalletAssetDto} to process
+     * @return List<CryptoItemDto>
+     */
+    public List<CryptoItemDto> fetchWalletPricesManuallyTriggered(UUID walletId, List<WalletAssetDto> listWalletAssets) {
+        Queue<WalletAssetDto> queue;
+        if (walletQueue.containsKey(walletId)) {
+            queue = walletQueue.get(walletId);
+        } else {
+            queue = new LinkedList<>();
+        }
+        queue.addAll(listWalletAssets);
+        walletQueue.put(walletId, queue);
+        return processInBatches(walletId, walletQueue.get(walletId));
+    }
+
+    /**
+     * Batch processing of queue items.
+     * Will receive a {@link Queue<WalletAssetDto>} and the walletId.
+     * Until the queue is empty  it will group the items in groups of {@link CryptoPriceService#numberOfThreadsPerWallet}, polling from the queue until the value is matched.
+     * To perform the batch of parallelized, it will create a List of {@link Mono} using {@link Mono#fromRunnable(Runnable)}.
+     * The list will be run using {@link Schedulers#boundedElastic()} that will assure that each request will run on a seperated thread.
+     * The {@link Mono#block(Duration)} in the end will ensure the requests are executed synchronously on the code block, before executing the next requests
+     *
+     * @param walletId UUID
+     * @param queue    Queue of WalletAssetDto
+     * @return List<CryptoPriceEntity>
+     */
+    private List<CryptoItemDto> processInBatches(UUID walletId, Queue<WalletAssetDto> queue) {
+        log.info("Now its {}", Instant.now());
+        List<CryptoPriceEntity> results = new ArrayList<>();
+
+        // Create a reactive queue (simulating fixed concurrency)
+        while (!queue.isEmpty()) {
+            // Collect the next 3 items in a batch
+            List<WalletAssetDto> batch = new ArrayList<>(numberOfThreadsPerWallet);
+            for (int i = 0; i < numberOfThreadsPerWallet && !queue.isEmpty(); i++) {
+                batch.add(queue.poll());
+            }
+
+            // Process the batch asynchronously with controlled concurrency
+            List<Mono<Object>> monos = batch.stream()
+                .map(asset -> Mono.fromRunnable(() -> {
+                        log.info("Submitted request {} at {}", asset.getSymbol(), Instant.now());
+                        try {
+                            if (StringUtils.hasText(asset.getExternalId())) {
+                                results.add(updateCryptoPrice(asset.getExternalId()));
+                            } else {
+                                results.add(updateCryptoPriceBySymbol(walletId, asset.getId(), asset.getSymbol()));
+                            }
+                        } catch (Exception e) {
+                            log.error("Error processing request for {}: {}", asset.getSymbol(), e.getMessage());
+                        }
+                    })
+                    .subscribeOn(Schedulers.boundedElastic())) // Use boundedElastic to limit concurrency
+                .toList();
+
+            // Merge all tasks in the batch and wait for completion
+            Mono.when(monos).block();
+        }
+
+        log.info("Results processed: {}", results.size());
+        return results.stream()
+            .map(CryptoPriceService::getCryptoItemDto)
+            .toList();
+    }
 
     /**
      * Method overload from {@link CryptoPriceService#getAssetLatestPrice(String, Instant)} without date.
